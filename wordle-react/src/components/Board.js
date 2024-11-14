a4f0fb3b510b1c33fdea5b5588b78df7ad6d0a31
Garrick Wong <inquisitor8986@gmail.com>
2024-11-14 13:31:12 -0700

    Slight bug fixes, still has some issues.

--- wordle-react/src/components/Board.js
+++ wordle-react/src/components/Board.js
@@ -3,24 +3,47 @@ import Box from "./Box.js"
 import React, { useState, useEffect } from 'react';
 import { boxDisplay, rowDisplay } from "./styles.js";
 
-//const wordList = ["BAKER", "TRADE", "CRANE"];
-const guessWord = "BAKER".split("");
+function checkWin(word, guessWord) {
+    let flags = [false, false, false, false, false];
+    let colors = ['white', 'white', 'white', 'white', 'white'];
+    for(let i = 0; i < 5; i++) {
+        if (word[i] === guessWord[i]) {
+            flags[i] = true;
+            colors[i] = 'green';
+        }
+    }
 
+    for (let i = 0; i < 5; i++) {
+        if (colors[i] === 'white') {
+          for (let j = 0; j < 5; j++) {
+            if (guessWord[j] === word[i] && !flags[j]) { //Triggers if flag that is being used isnt used
+              colors[i] = 'yellow';
+              flags[j] = true;
+              break;
+            }
+          }
+        }
+    }
+    console.log("Word to be guessed", guessWord);
+    console.log("Word", word);
+    return colors;
+}
+
+//const num = Math.floor(Math.random()*3103); //Get a random number to choose a word.
+//const guessWord = wordList[num];
+const guessWord = "BAKER";
 function Board() {
-
-    const[colors, setColors] = useState(Array(6).fill(Array(5).fill('white')));
-    const[words, setWord] = useState(Array(6).fill(Array(5).fill(null)));
+    const[colors, setColors] = useState(Array(6).fill(Array(5).fill('white'))); //2d array to hold the color hints
+    const[words, setWord] = useState(Array(6).fill(Array(5).fill(null))); //A 2d array to hold the words
     const[index, setIndex] = useState(-1); //Sets the index of the current word
     const[currRow, setCurrRow] = useState(0); //Sets the current word
-    //const[isLocked, setIsLocked] = useState(false);
-
     const handleKeyDown = (e) => {
-        if(e.key.length === 1 && /^[a-zA-Z]$/.test(e.key)) {
+        if(e.key.length === 1 && /^[a-zA-Z]$/.test(e.key)) { //Check if key pressed is a letter
             setIndex((prevIndex) => {
-                const newIndex = Math.min(prevIndex + 1, 4);
+                const newIndex = Math.min(prevIndex + 1, 4); //Set new index
                 const newLetter = e.key.toUpperCase();
                 setWord((prevWords) => {
-                    const newWords = prevWords.map((word, wordIndex) => {
+                    const newWords = prevWords.map((word, wordIndex) => { //Generate new word
                         if(wordIndex === currRow) {
                             const newWord = [...word];
                             newWord[newIndex] = newLetter;
@@ -35,7 +58,6 @@ function Board() {
             
             
         } else if (e.key === 'Enter' && index === 4) {
-            //setIsLocked(true);
             setColors((prevColors) => {
                 const colorHint =  checkWin(words[currRow], guessWord);
                 console.log(colorHint);
@@ -54,6 +76,7 @@ function Board() {
                 const newIndex = -1;
                 return newIndex;
             });
+
         } else if (e.key === 'Backspace') {
             setIndex((prevIndex) => {
                 const newIndex = Math.max(prevIndex - 1, -1);
@@ -75,11 +98,6 @@ function Board() {
 
     useEffect(() => {
         window.addEventListener('keydown', handleKeyDown);
-        // if (isLocked) {
-        //     window.removeEventListener('keydown', handleKeyDown);
-        // } else {
-        //     window.addEventListener('keydown', handleKeyDown);
-        // }
         return () => {
             window.removeEventListener('keydown', handleKeyDown);
         };
@@ -97,7 +115,7 @@ function Board() {
                 </div>
                 <div style ={boxDisplay}>
                     <Box value = {words[1][0]} color = {colors[1][0]}/>
-                    <Box value = {words[1][1]} color = {colors[1][0][1]}/>
+                    <Box value = {words[1][1]} color = {colors[1][1]}/>
                     <Box value = {words[1][2]} color = {colors[1][2]}/>
                     <Box value = {words[1][3]} color = {colors[1][3]}/>
                     <Box value = {words[1][4]} color = {colors[1][4]}/>
@@ -136,29 +154,3 @@ function Board() {
 }
 
 export default Board;
-
-function checkWin(word, guessWord) {
-    let flags = [false, false, false, false, false];
-    let colors = ['white', 'white', 'white', 'white', 'white'];
-    for(let i = 0; i < 5; i++) {
-        if (word[i] === guessWord[i]) {
-            flags[i] = true;
-            colors[i] = 'green';
-        }
-    }
-
-    for (let i = 0; i < 5; i++) {
-        if (colors[i] === 'white') {
-          for (let j = 0; j < 5; j++) {
-            if (guessWord[i] === word[j] && !flags[j]) { //Triggers if flag that is being used isnt used
-              colors[i] = 'yellow';
-              flags[j] = true;
-              break;
-            }
-          }
-        }
-    }
-    console.log("Word to be guessed", guessWord);
-    console.log("Word", word);
-    return colors;
-}
